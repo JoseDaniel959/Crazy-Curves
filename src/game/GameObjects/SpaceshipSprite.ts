1aa3adc2244b496191173afa6e0469718aa169b8
Jose Daniel <jobustamantea@unale.edu.co>
2026-04-12 21:47:51 -0500

    moviendo la logica del moviento a la clase SpaceshipSprite.ts

--- src/game/GameObjects/SpaceshipSprite.ts
+++ src/game/GameObjects/SpaceshipSprite.ts
@@ -1,6 +1,31 @@
+import { Physics } from "phaser"
+
 export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string){
-        super(scene,x,y,texture)
+    constructor(scene: Phaser.Scene, x: number, y: number){
+        super(scene,x,y,"Spaceship")
+        
+        //adding the sprite to the scene
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+        
+        //setting sprite's hitbox
+        this.setScale(0.10)
+        this.setCircle(70,200,180)
+        
+    }
+    
+    idle(){
+        this.setAngularVelocity(0);
+    }
+
+    moveLeft(){
+        this.setAngularVelocity(100)
+          console.log("ME PRESIONARON")
+    }
+
+    moveRight(){
+        this.setAngularVelocity(-100)
+          console.log("ME PRESIONARON")
     }
     
     
--- src/game/Scenes/MainGame.ts
+++ src/game/Scenes/MainGame.ts
@@ -11,30 +11,24 @@ export default class BootLoader extends Phaser.Scene{
     }
     create(){
         this.cursors = this.input.keyboard?.createCursorKeys();
-        this.jugador =  this.add.existing(new SpaceshipSprite(this,100,100,"Spaceship").setScale(0.1))
-        this.jugador = this.physics.add.existing(this.jugador,false)
-    
+        this.jugador =  new SpaceshipSprite(this,100,100)
         const music = this.sound.add('music1').setVolume(0.3);
         music.play()    
     }
     update(time: number, delta: number): void {
         if(this.jugador && this.jugador.body) this.physics.velocityFromAngle(this.jugador.angle , 150, this.jugador.body.velocity);
-        
          
         if (this.cursors?.left.isUp ||this.cursors?.right.isUp )
         {
-            this.jugador?.setAngularVelocity(0)
-            console.log("velocidad angular 0")
+            this.jugador?.idle()
         }
         if (this.cursors?.left.isDown)
         {
-            this.jugador?.setAngularVelocity(100)
-            console.log("ME PRESIONARON")
+            this.jugador?.moveLeft()
         }
-          if (this.cursors?.right.isDown)
+        if (this.cursors?.right.isDown)
         {
-            this.jugador?.setAngularVelocity(-100)
-            console.log("ME PRESIONARON")
+            this.jugador?.moveRight()
         }
     }
 }
