ba762f932c78b13f61c3353d025f3ec7ba637c61
Jose Daniel <jobustamantea@unale.edu.co>
2026-04-18 21:58:31 -0500

    Refactor y creación de lógica para la generación de la estela y el chequeo de colisiones de la nave con la estela

--- src/game/GameObjects/SpaceshipSprite.ts
+++ src/game/GameObjects/SpaceshipSprite.ts
@@ -1,32 +1,67 @@
-import { Physics } from "phaser"
+import TailSprite from "./TailSprite";
 
-export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene: Phaser.Scene, x: number, y: number){
-        super(scene,x,y,"Spaceship")
-        
+export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+    private offset = -20;
+    private tail: TailSprite[] = [];
+    
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, "Spaceship")
+
         //adding the sprite to the scene
         scene.add.existing(this)
-        scene.physics.add.existing(this)
-        
+        scene.physics.add.existing(this, false)
+         
+        //setting scenes input
+        this.cursors = scene.input.keyboard?.createCursorKeys();
+
         //setting sprite's hitbox
         this.setScale(0.10)
-        this.setCircle(70,200,180)
+        this.setCircle(70, 200, 180)
+        this.setOrigin(0.5, 0.5)
+
         
+        //setting a random angle         
+        this.angle = Phaser.Math.Angle.RandomDegrees();
+
     }
-    
-    idle(){
+
+    idle() {
         this.setAngularVelocity(0);
     }
 
-    moveLeft(){
+    moveLeft() {
         this.setAngularVelocity(100)
-          console.log("ME PRESIONARON")
     }
 
-    moveRight(){
+    moveRight() {
         this.setAngularVelocity(-100)
-          console.log("ME PRESIONARON")
     }
-    
-    
+
+    addLine(){
+        this.tail.push(new TailSprite(this.scene, this.x + Math.cos(this.rotation) * this.offset, this.y + Math.sin(this.rotation) * this.offset));
+        this.scene.physics.collide(this, this.tail,()=> console.log("COLISION!!!!!"));
+    }
+
+    checkCollisitions(){
+
+    }
+
+
+    move() {
+        this.addLine()
+        this.scene.physics.velocityFromAngle(this.angle, 150, this.body?.velocity)
+
+        if (this.cursors?.left.isUp || this.cursors?.right.isUp) {
+            this.idle()
+        }
+        if (this.cursors?.left.isDown) {
+            this.moveRight()
+        }
+        if (this.cursors?.right.isDown) {
+            this.moveLeft()
+        }
+    }
+
+
 }
