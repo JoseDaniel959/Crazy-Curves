b8983e1401ff55dd9d80e17e62ff4d43bd04f92e
Jose Daniel <jobustamantea@unale.edu.co>
2026-04-25 00:21:52 -0500

    se añade explosion cuando la nave choca con su propia cola

--- src/game/GameObjects/SpaceshipSprite.ts
+++ src/game/GameObjects/SpaceshipSprite.ts
@@ -1,3 +1,4 @@
+import ExplodeSprite from "./ExplodeSprite";
 import TailSprite from "./TailSprite";
 import BulletProjectil from "./Weapons/BulletProjectile";
 
@@ -5,14 +6,14 @@ export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
     private offset = -20;
     private tail: TailSprite[] = [];
-    
+
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, "Spaceship")
 
         //adding the sprite to the scene
         scene.add.existing(this)
         scene.physics.add.existing(this, false)
-         
+
         //setting scenes input
         this.cursors = scene.input.keyboard?.createCursorKeys();
 
@@ -21,47 +22,60 @@ export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
         this.setCircle(70, 200, 180)
         this.setOrigin(0.5, 0.5)
 
-        
+
         //setting a random angle         
         this.angle = Phaser.Math.Angle.RandomDegrees();
 
+        // 
+        this.setCollideWorldBounds(true);
     }
 
-    idle():void {
+    idle(): void {
         this.setAngularVelocity(0);
     }
 
-    moveLeft():void {
+    moveLeft(): void {
         this.setAngularVelocity(100)
     }
 
-    moveRight():void {
+    moveRight(): void {
         this.setAngularVelocity(-100)
     }
 
-    addLine():void{
+    addLine(): void {
         this.tail.push(new TailSprite(this.scene, this.x + Math.cos(this.rotation) * this.offset, this.y + Math.sin(this.rotation) * this.offset));
     }
 
-    checkCollisions():void{
-        this.scene.physics.collide(this, this.tail,()=> console.log("COLISION!!!!!"));
+    checkCollisions() {
+        return this.scene.physics.collide(this, this.tail, () => true);
     }
 
 
     move() {
-        this.checkCollisions();
-        this.addLine()
-        this.scene.physics.velocityFromAngle(this.angle, 150, this.body?.velocity)
+        if (!this.checkCollisions()) {
+            this.addLine()
+            this.scene.physics.velocityFromAngle(this.angle, 150, this.body?.velocity)
 
-        if (this.cursors?.left.isUp || this.cursors?.right.isUp) {
-            this.idle()
+            if (this.cursors?.left.isUp || this.cursors?.right.isUp) {
+                this.idle()
+            }
+            if (this.cursors?.left.isDown) {
+                this.moveRight()
+            }
+            if (this.cursors?.right.isDown) {
+                this.moveLeft()
+            }
         }
-        if (this.cursors?.left.isDown) {
-            this.moveRight()
+        else{
+            this.explode(this.x,this.y)
+            this.disableBody(true,true)
         }
-        if (this.cursors?.right.isDown) {
-            this.moveLeft()
-        }
+
+    }
+
+
+    explode(x: number, y: number): ExplodeSprite {
+        return new ExplodeSprite(this.scene, this.x, this.y)
     }
 
 
--- src/game/Scenes/MainGame.ts
+++ src/game/Scenes/MainGame.ts
@@ -11,18 +11,12 @@ export default class BootLoader extends Phaser.Scene {
         this.jugador = new SpaceshipSprite(this, 100, 100)
         const music = this.sound.add('music1').setVolume(0.3);
         music.play()
-        const animation =  this.anims.create({
-            key:"Explosion",
-            frames: this.anims.generateFrameNumbers('Explosion'),
-             frameRate: 32
-        })
-
-         const sprite = this.add.sprite(50, 300, 'Explosion').setScale(0.1);
-
-         sprite.play({ key: 'Explosion', repeat: 7 });
     }
     update(time: number, delta: number): void {
-        this.jugador?.move();
+        if(this.jugador){
+            this.jugador?.move();
+        } 
+       
 
 
     }
