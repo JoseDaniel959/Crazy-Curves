7f66db7dece929aaeb7515ed276eb4e84a915fe7
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-03 16:53:12 -0500

    se crea clase JumpPower y se usa en SpaceshipSprite

--- src/game/GameObjects/SpaceshipSprite.ts
+++ src/game/GameObjects/SpaceshipSprite.ts
@@ -1,12 +1,15 @@
 import ExplodeSprite from "./ExplodeSprite";
+import JumpPower from "./Powers/JumpPower";
 import TailSprite from "./TailSprite";
 
 export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
     private offset = -20;
     private tail: TailSprite[] = [];
-    private elapsedTime: number = 250; 
+    private elapsedTime: number = 250;
     private randomNumber: number = 20;
+    private downPower: JumpPower;
+    private isCheckCollisionsOn: boolean = true;
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, "Spaceship")
 
@@ -26,6 +29,12 @@ export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
         this.angle = Phaser.Math.Angle.RandomDegrees();
 
         this.setCollideWorldBounds(true, 0, 0, true);
+
+        this.downPower = new JumpPower(this);
+
+        //setting the depth of the object. This is for the JumpPower
+        this.setDepth(1)
+
     }
 
     idle(): void {
@@ -40,30 +49,34 @@ export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
         this.setAngularVelocity(-165)
     }
 
-    addLine(deltaTime:number): void {
-        if(this.elapsedTime === 250){
-            this.randomNumber =Phaser.Math.Between(0, 50);
-           this.tail.push(new TailSprite(this.scene, this.x + Math.cos(this.rotation) * this.offset, this.y + Math.sin(this.rotation) * this.offset));
+    addLine(deltaTime: number): void {
+        if (this.elapsedTime === 250) {
+            this.randomNumber = Phaser.Math.Between(0, 50);
+            this.tail.push(new TailSprite(this.scene, this.x + Math.cos(this.rotation) * this.offset, this.y + Math.sin(this.rotation) * this.offset));
         }
-        if(this.randomNumber === 1){
+        if (this.randomNumber === 1) {
             this.updateTime(deltaTime)
         }
-        console.log(this.randomNumber)
     }
 
-    checkTailCollisions(): boolean {
-        return this.scene.physics.collide(this, this.tail, () => true);
+    checkTailCollisions(isOn: boolean): void {
+        if (isOn) {
+            this.scene.physics.collide(this, this.tail, () => {
+                this.explode(this.x, this.y)
+                this.disableBody(true, true)
+
+            });
+        }
+
     }
 
-    updateTime(deltaTime:number):void{
-       if(this.elapsedTime > 0){
+    updateTime(deltaTime: number): void {
+        if (this.elapsedTime > 0) {
             this.elapsedTime -= deltaTime
-            console.log(this.elapsedTime )
-
-       }
-       else{
-         this.elapsedTime = 250;
-       }
+        }
+        else {
+            this.elapsedTime = 250;
+        }
     }
 
     checkWordBoundsCollisions() {
@@ -72,27 +85,31 @@ export default class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite {
             this.disableBody(true, true)
         });
     }
+    
+    setIsCheckCollisionsOn(value : boolean){
+        this.isCheckCollisionsOn = value;
+    }
 
-    move(deltaTime:number) {
+    move(deltaTime: number) {
         this.checkWordBoundsCollisions()
-        if (!this.checkTailCollisions()) {
-            this.addLine(deltaTime)
-            this.scene.physics.velocityFromAngle(this.angle, 150, this.body?.velocity)
+        this.checkTailCollisions(this.isCheckCollisionsOn)
+        this.addLine(deltaTime)
+        this.scene.physics.velocityFromAngle(this.angle, 150, this.body?.velocity)
 
-            if (this.cursors?.left.isUp || this.cursors?.right.isUp) {
-                this.idle()
-            }
-            if (this.cursors?.left.isDown) {
-                this.moveRight()
-            }
-            if (this.cursors?.right.isDown) {
-                this.moveLeft()
-            }
+        if (this.cursors?.left.isUp || this.cursors?.right.isUp) {
+            this.idle()
         }
-        else {
-            this.explode(this.x, this.y)
-            this.disableBody(true, true)
+        if (this.cursors?.left.isDown) {
+            this.moveRight()
         }
+        if (this.cursors?.right.isDown) {
+            this.moveLeft()
+        }
+        if (this.cursors?.down.isDown) {
+            this.downPower.physics()
+
+        }
+
 
     }
 
