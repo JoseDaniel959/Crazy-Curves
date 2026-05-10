8ad5a89f1dc3708ce0bc15f9adb73baeb4d070ec
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-09 22:23:24 -0500

    Archivo para objetos dom

---
+++ src/components/Menu/MenuElements.ts
@@ -0,0 +1,4 @@
+import DomElement from "../DomElement";
+export const gameTitleCSS: string = `
+font-family: 'Honk';`
+const gameTitle: DomElement = new DomElement(400, 0, 'h1', gameTitleCSS, 'Crazy Curves');
