7fb4216d400cb416a4ba59ae97e0944ddd31d9eb
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-14 15:04:30 -0500

    se añade algunos atributos como optional

--- src/components/DomElement.ts
+++ src/components/DomElement.ts
@@ -2,15 +2,15 @@ export default class DomElement{
     x : number;
     y : number;
     element: string;
-    style : string;
-    text: string
+    style? : string;
+    text?: string
     
     constructor(
         x : number,
         y : number,
         element: string,
-        style : string,
-        text: string){
+        style? : string,
+        text?: string){
         this.x = x;
         this.y = y;
         this.element = element;
