4c92eb2893dd88ff0c5baa61f14f1061d473104d
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-14 15:04:12 -0500

    función para añadir jugadores al menu principal

--- src/game/Scenes/MainMenu.ts
+++ src/game/Scenes/MainMenu.ts
@@ -1,18 +1,21 @@
-export default class MainMenu extends Phaser.Scene
-{
+import { gameTitle,playerBox, playerDiv, } from '../../components/Menu/MenuElements';
+import styles from '../../components/Menu/styles.css'
+export default class MainMenu extends Phaser.Scene{
     constructor ()
     {
         super('MainMenu');
     }
+    preload(){
+    }
 
     create(){
-      
-        const element = this.add.dom(200,200,"button",'background-color: lime; width: 220px; height: 100px; font: 48px Arial', "Start")
-        element.setInteractive();
-     
-        element.once('pointerdown', () => {
-            this.scene.start("MainGame")
-        });
+        
+        playerDiv(this,"HandsomeMCT",0)
+        playerDiv(this,"HandsomeMCT",80)
+
+
+
+
         
     }
 
