d5496f56bc0d76ad2bf6c6a5a7ef477cf71b6ba4
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-14 23:01:36 -0500

    fixing import

--- game/src/game/Scenes/MainMenu.ts
+++ game/src/game/Scenes/MainMenu.ts
@@ -1,5 +1,4 @@
 import { gameTitle, playerBox, playerDiv, } from '../../components/Menu/MenuElements';
-import styles from '../../components/Menu/styles.css'
 import { emitNewPlayerInMenu, socket } from '../socket/socketFunctions';
 export default class MainMenu extends Phaser.Scene {
     constructor() {
