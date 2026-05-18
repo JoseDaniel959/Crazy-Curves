1a7e81b0717895e269abb02e9979b893bdf5d3c2
Jose Daniel <jobustamantea@unale.edu.co>
2026-05-17 22:38:39 -0500

    Creaación de PowerSelectionComponent

---
+++ game/src/game/components/Menu/CompoundComponents/PowerSelectionComponent.ts
@@ -0,0 +1,18 @@
+import ButtonComponent from "../AtomicComponents/ButtonComponent";
+import UIComponent from "../UIComponent";
+
+export default class PowerSelectionComponent extends UIComponent {;
+    backwardButton: ButtonComponent;
+    forwardButton: ButtonComponent;
+
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, backWardButton: ButtonComponent, forwardButton: ButtonComponent,) {
+        super(scene,x,y,texture);
+        this.backwardButton = backWardButton;
+        this.forwardButton = forwardButton;
+
+        scene.add.image(x, y, 'BackwardButton').setScale(0.15)
+    }
+
+
+}
