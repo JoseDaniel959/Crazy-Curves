b8f141a82b9dfe8ac4b542668606246de2e14f8d
Jose Daniel <jobustamantea@unal.edu.co>
2026-05-21 22:10:42 -0500

    comentando codigo

--- game/src/game/UI/Menu/MenuElements.ts
+++ game/src/game/UI/Menu/MenuElements.ts
@@ -17,9 +17,9 @@ export const boxCSS: string = `
 
 export const gameTitle: DomElement = new DomElement(400, 0, 'h1', gameTitleCSS, 'Crazy Curves');
 const startButton: DomElement = new DomElement(400, 550, 'button', undefined, "start")
-export const playerTable = (scene: Phaser.Scene, playerName: string, offset: number): void => {
-            new PlayerSelectionComponent(scene,500,200,1)
-}
+// export const playerTable = (scene: Phaser.Scene, playerName: string, offset: number): void => {
+//             new PlayerSelectionComponent(scene,500,200,)
+// }
 
 
 // This displays the power inside the playerTable component
