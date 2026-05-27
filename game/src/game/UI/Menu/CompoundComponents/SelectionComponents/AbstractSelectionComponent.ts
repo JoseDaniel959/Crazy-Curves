77a239f706f1b34e8c0efcc73ac87ccb8fd3060e
Jose Daniel <jobustamantea@unal.edu.co>
2026-05-27 16:30:53 -0500

    se añade getter and setter, logica de renderizado y métodos abstractos

--- game/src/game/UI/Menu/CompoundComponents/SelectionComponents/AbstractSelectionComponent.ts
+++ game/src/game/UI/Menu/CompoundComponents/SelectionComponents/AbstractSelectionComponent.ts
@@ -1,30 +1,64 @@
+import ButtonComponent from "../../AtomicComponents/ButtonComponent";
 import UIComponent from "../../UIComponent";
 
 export default abstract class AbstractSelectionComponent extends UIComponent {
+    private currentIndex: number = 0;
     private texturesKeyArray: string[] = [];
     private textureStartsWith: string;
+    private BackwardButton: ButtonComponent;
+    private ForwardButton: ButtonComponent;
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, scale: number = 1, textureStartsWith: string) {
         super(scene, x, y, texture, scale);
         this.textureStartsWith = textureStartsWith;
-        this.setTexturesKeyArray(this.scene.textures.getTextureKeys().filter((textureKey)=> textureKey.startsWith(this.textureStartsWith)))
+        this.setTexturesKeyArray(this.scene.textures.getTextureKeys().filter((textureKey) => textureKey.startsWith(this.textureStartsWith)))
         //this is the current texture the user is selecting
-        this.texture = this.texturesKeyArray[0]
+        this.texture = this.texturesKeyArray[this.currentIndex]
         console.log(this.texturesKeyArray)
-    }
 
-    public getTexturesKeyArray(): string[]{
+        this.BackwardButton = new ButtonComponent(scene, x - 12, y + 28, 'BackwardButton', 0.1, () => this.previousTextureInArray())
+        this.ForwardButton = new ButtonComponent(scene, x + 12, y + 28, 'ForwardButton', 0.1, () => this.nextTextureInArray())
+    }
+    abstract nextTextureInArray(): void
+
+    abstract previousTextureInArray() : void
+
+    public getTexturesKeyArray(): string[] {
         return this.texturesKeyArray;
     }
 
-    public setTexturesKeyArray(newtexturesKeyArray: string[]): void{
+    public setTexturesKeyArray(newtexturesKeyArray: string[]): void {
         this.texturesKeyArray = newtexturesKeyArray;
     }
 
-    public getTextureStartsWith(): string{
+    public getTextureStartsWith(): string {
         return this.textureStartsWith;
     }
 
-    public setTextureStartsWith(newTextureStartsWith: string):void{
+    public setTextureStartsWith(newTextureStartsWith: string): void {
         this.textureStartsWith = newTextureStartsWith;
     }
+
+    public getCurrentIndex(): number {
+        return this.currentIndex;
+    }
+
+    public setCurrentIndex(newIndex: number): void {
+        this.currentIndex = newIndex;
+    }
+    public getBackwardButton(): ButtonComponent {
+        return this.BackwardButton;
+    }
+
+    public setBackwardButton(backwardButton: ButtonComponent): void {
+        this.BackwardButton = backwardButton;
+    }
+
+    public getForwardButton(): ButtonComponent {
+        return this.ForwardButton;
+    }
+
+    public setForwardButton(forwardButton: ButtonComponent): void {
+        this.ForwardButton = forwardButton;
+    }
+
 }
