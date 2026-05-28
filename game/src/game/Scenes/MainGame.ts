bf741176b4b6c947276abded3e5bc008814bbbfb
Jose Daniel <jobustamantea@unal.edu.co>
2026-05-28 14:13:36 -0500

    pasando datos de la seleccion del usuario al registry

--- game/src/game/Scenes/MainGame.ts
+++ game/src/game/Scenes/MainGame.ts
@@ -1,18 +1,26 @@
 import Phaser from "phaser"
 import SpaceshipSprite from "../GameObjects/SpaceshipSprite"
+import { registryKey } from "../Registry/RegistryKeys";
 
 
 
 export default class BootLoader extends Phaser.Scene {
     private jugador: SpaceshipSprite | undefined;
+
     constructor() {
         super('MainGame')
+
 
     }
     create() {
         this.add.image(400,300,"Background").setToBack()
         const music = this.sound.add('music1').setVolume(0.3);
-        this.jugador = new SpaceshipSprite(this,200,200);
+        
+        //Getting spaceship texture and tail texture from data manager
+        const spaceshipTextureKey : string  = this.registry.get(registryKey.playerSelectionData).spaceshipTexturekey;
+        const tailTextureKey: string = this.registry.get(registryKey.playerSelectionData).tailComponenteTextureKey
+        
+        this.jugador = new SpaceshipSprite(this,200,200,spaceshipTextureKey,tailTextureKey);
         music.play()
         
     }
--- game/src/game/Scenes/MainMenu.ts
+++ game/src/game/Scenes/MainMenu.ts
@@ -4,23 +4,35 @@ import { socket } from '../../Socket/socketFunctions';
 import { ServerSocketEvents } from '../../Socket/ServerSocketEvents';
 import { ClientSocketEvents } from '../../Socket/ClientSocketEvents';
 import ButtonComponent from '../UI/Menu/AtomicComponents/ButtonComponent';
+import { registryKey } from '../Registry/RegistryKeys';
 export default class MainMenu extends Phaser.Scene {
+    private playerSelectionComponent: PlayerSelectionComponent | undefined;
     constructor() {
         super('MainMenu');
-    }
-    preload() {
     }
     create() {
         socket.emit(ClientSocketEvents.newPlayer)
         socket.on(ServerSocketEvents.getAllPlayers, (data: string[]) => {
             let offset = 0
             for (let id in data) {
-                new PlayerSelectionComponent(this, 500, 200 + offset)
+                new PlayerSelectionComponent(this, 650, 200 + offset)
                 offset += 150;
             }
         })
-        new PlayerSelectionComponent(this, 500, 200)
+        this.playerSelectionComponent = new PlayerSelectionComponent(this, 500, 200)
         new ButtonComponent(this, 500, 950, "StartButton", 0.5, () => {
+            console.log()
+            this.registry.set(registryKey.playerSelectionData,
+                {
+                    spaceshipTexturekey: this.playerSelectionComponent?.
+                        getSpaceshipSelectionComponent()
+                        .getTexture(),
+
+                    tailComponenteTextureKey: this.playerSelectionComponent?.
+                        getTailSelectionComponent()
+                        .getTexture(),
+
+                })
             this.scene.start("MainGame")
         })
     }
