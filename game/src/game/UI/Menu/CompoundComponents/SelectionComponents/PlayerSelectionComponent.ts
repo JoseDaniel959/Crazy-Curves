e98cf8091313d758bd8e3de7e642909c5ed29089
Jose Daniel <jobustamantea@unal.edu.co>
2026-05-28 14:12:41 -0500

    añadiendo properties a PlayerSelectionComponent

--- game/src/game/UI/Menu/CompoundComponents/SelectionComponents/PlayerSelectionComponent.ts
+++ game/src/game/UI/Menu/CompoundComponents/SelectionComponents/PlayerSelectionComponent.ts
@@ -5,6 +5,10 @@ import SpaceshipSelectionComponent from "./SpaceshipSelectionComponent";
 import TailSelectionComponent from "./TailSelectionComponent";
 
 export default class PlayerSelectionComponent extends UIComponent {
+    private spaceshipSelectionComponent: SpaceshipSelectionComponent;
+    private upPowerSelectionComponent: PowerSelectionComponent;
+    private downPowerSelectionComponent: PowerSelectionComponent;
+    private tailSelectionComponent: TailSelectionComponent;
 
 
     constructor(
@@ -16,10 +20,49 @@ export default class PlayerSelectionComponent extends UIComponent {
         new TableComponent(scene, x, y, 1.5)
 
         scene.add.text(x + 50, y - 15, "HandSomeMCT")
-        new SpaceshipSelectionComponent(scene, x - 170, y, 0.18, "Spaceship")
-        new PowerSelectionComponent(scene, x - 50, y - 50, "BackwardButton", 0.15)
-        new PowerSelectionComponent(scene, x - 50, y + 10, "BackwardButton", 0.15)
-        new TailSelectionComponent(scene, x - 110, y - 20, 0.5, "tail")
+        this.spaceshipSelectionComponent = new SpaceshipSelectionComponent(scene, x - 170, y, 0.18, "Spaceship")
+        this.upPowerSelectionComponent = new PowerSelectionComponent(scene, x - 50, y - 50, "BackwardButton", 0.15)
+        this.downPowerSelectionComponent = new PowerSelectionComponent(scene, x - 50, y + 10, "BackwardButton", 0.15)
+        this.tailSelectionComponent = new TailSelectionComponent(scene, x - 110, y - 20, 0.5, "tail")
+    }
 
+    public getSpaceshipSelectionComponent(): SpaceshipSelectionComponent {
+        return this.spaceshipSelectionComponent;
+    }
+
+    public setSpaceshipSelectionComponent(
+        spaceshipSelectionComponent: SpaceshipSelectionComponent
+    ): void {
+        this.spaceshipSelectionComponent = spaceshipSelectionComponent;
+    }
+
+    public getUpPowerSelectionComponent(): PowerSelectionComponent {
+        return this.upPowerSelectionComponent;
+    }
+
+    public setUpPowerSelectionComponent(
+        upPowerSelectionComponent: PowerSelectionComponent
+    ): void {
+        this.upPowerSelectionComponent = upPowerSelectionComponent;
+    }
+
+    public getDownPowerSelectionComponent(): PowerSelectionComponent {
+        return this.downPowerSelectionComponent;
+    }
+
+    public setDownPowerSelectionComponent(
+        downPowerSelectionComponent: PowerSelectionComponent
+    ): void {
+        this.downPowerSelectionComponent = downPowerSelectionComponent;
+    }
+
+    public getTailSelectionComponent(): TailSelectionComponent {
+        return this.tailSelectionComponent;
+    }
+
+    public setTailSelectionComponent(
+        tailSelectionComponent: TailSelectionComponent
+    ): void {
+        this.tailSelectionComponent = tailSelectionComponent;
     }
 }
