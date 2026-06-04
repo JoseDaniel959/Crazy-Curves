e55901d73b6457b17ac49caff632ce5bd26d874a
Jose Daniel <jobustamantea@unal.edu.co>
2026-06-03 21:44:59 -0500

    Refactor del archivo y se crea un array para la gestión de todos los usuarios en la aplicación

--- game/src/Socket/socketFunctions.ts
+++ game/src/Socket/socketFunctions.ts
@@ -1,2 +1,24 @@
 import { io, Socket } from "socket.io-client";
+import { ServerSocketEvents } from "./ServerSocketEvents";
+import PlayerSession from "../playerSession/PlayerSession";
+import { PlayerSessionDTO } from "../game/DTO/DTOTypes";
 export const socket: Socket = io();
+
+export let playersOnline: PlayerSessionDTO[] = [];
+
+
+socket.on(ServerSocketEvents.getAllPlayers, (data) =>{
+    console.log("nuevos usuarios")
+    playersOnline = data;
+    console.log(playersOnline)
+})
+
+
+socket.on(ServerSocketEvents.removePlayerFromMenu,(playerId) =>{
+    console.log("eliminar del array el player con id ", playerId)
+    console.log(playerId)
+    playersOnline = playersOnline.filter((playerOnline) => playerOnline.playerId !== playerId)
+    console.log(playersOnline)
+})
+
+
