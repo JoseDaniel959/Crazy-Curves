f5a6ca60438d3945b9a730087c01f6ddb1750f24
Jose Daniel <jobustamantea@unal.edu.co>
2026-06-03 21:45:44 -0500

    Refactor de eventos y se crea evento

--- game/src/Socket/ServerSocketEvents.ts
+++ game/src/Socket/ServerSocketEvents.ts
@@ -1,4 +1,5 @@
 export const ServerSocketEvents = {
+    playerCreated: "playerCreated",
     addNewPlayerInMenu: "addNewPlayerInMenu",
     getAllPlayers: "getAllPlayers",
     removePlayerFromMenu: "removePlayerFromMenu"
