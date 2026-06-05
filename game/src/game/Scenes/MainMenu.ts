0b492a68a2f3d40098784ebec385cb35dd64068c
Jose Daniel <jobustamantea@unal.edu.co>
2026-06-05 06:40:34 -0500

    se añade funcionalidad para mostrar jugadores conectados

--- game/src/game/Scenes/MainMenu.ts
+++ game/src/game/Scenes/MainMenu.ts
@@ -1,45 +1,80 @@
 import { gameTitle, } from '../UI/Menu/MenuElements';
 import PlayerSelectionComponent from '../UI/Menu/CompoundComponents/SelectionComponents/PlayerSelectionComponent';
-import { socket } from '../../Socket/socketFunctions';
+import { playersOnline, socket } from '../../Socket/socketFunctions';
 import { ServerSocketEvents } from '../../Socket/ServerSocketEvents';
 import { ClientSocketEvents } from '../../Socket/ClientSocketEvents';
 import ButtonComponent from '../UI/Menu/AtomicComponents/ButtonComponent';
 import { registryKey } from '../Registry/RegistryKeys';
+import PlayerSession from '../../playerSession/PlayerSession';
+import { PlayerSessionDTO } from '../DTO/DTOTypes';
 export default class MainMenu extends Phaser.Scene {
-    private playerSelectionComponent: PlayerSelectionComponent | undefined;
+
     constructor() {
         super('MainMenu');
     }
-    // private starButtonCallBack() {
-    //     this.registry.set(registryKey.playerSelectionData,
-    //         {
-    //             spaceshipTexturekey: this.playerSelectionComponent?.
-    //                 getSpaceshipSelectionComponent()
-    //                 .getTexture(),
 
-    //             tailComponenteTextureKey: this.playerSelectionComponent?.
-    //                 getTailSelectionComponent()
-    //                 .getTexture(),
-
-    //         })
-    //     this.scene.start("MainGame")
-    // }
-    
     create() {
-        const jugador = new PlayerSelectionComponent(this, 500, 350);
-        // jugador.listener()
-        socket.emit(ClientSocketEvents.newPlayer)
-        socket.on(ServerSocketEvents.getAllPlayers, (data: string[]) => {
+        let playersSelectionComponent: PlayerSelectionComponent[] = [];
+
+        socket.emit(ClientSocketEvents.getAllPlayers)
+        
+        socket.on(ServerSocketEvents.getAllPlayers, () => {
             let offset = 0
-            for (let id in data) {
-                new PlayerSelectionComponent(this, 500, 350 + offset)
+
+            playersOnline.forEach((playerOnline: PlayerSessionDTO) => {
+                const { playerId, playerName } = playerOnline
+                console.log("playerSelectionComponent")
+                console.log(playersSelectionComponent)
+
+                if (!this.isPlayerConnected(playersSelectionComponent, playerId)) {
+
+
+                    playersSelectionComponent.push(
+                        new PlayerSelectionComponent(this, 500, 350 + offset, playerName, playerId)
+                    )
+                }
                 offset += 150;
-            }
+            })
+
         })
 
-        new ButtonComponent(this, 500, 950, "StartButton", 0.5, ()=>{
-            jugador.destroy()
+        //removing player from the menu
+        socket.on(ServerSocketEvents.removePlayerFromMenu, (playerId:string) =>{
+            console.log("desconecto a un usuario")
+            this.disconectPlayer(playersSelectionComponent,playerId)
+        })
+
+
+        new ButtonComponent(this, 500, 950, "StartButton", 0.5, () => {
+            this.scene.start('MainGame')
         })
     }
+
+    isPlayerConnected(playersSelectionComponent: PlayerSelectionComponent[], playerIdToSeach: string): boolean {
+        const playerSelectionFound = playersSelectionComponent.find((PlayerSelectionComponent) => {
+            if (PlayerSelectionComponent.playerId === playerIdToSeach) {
+                return playersSelectionComponent;
+            }
+        })
+        return playerSelectionFound ? true : false;
+    }
+
+    disconectPlayer(playersSelectionComponent: PlayerSelectionComponent[], playerIdToDisconnect: string) {
+       playersSelectionComponent.some((playerSelectionComponent) => {
+            if (playerSelectionComponent.playerId == playerIdToDisconnect) {
+                 playerSelectionComponent.destroy();
+            }
+        })
+    }
+
+    // public getPlayersSelectionComponent(): PlayerSelectionComponent[] | undefined {
+    //     return this.playersSelectionComponent;
+    // }
+
+    // public setPlayersSelectionComponent(
+    //     playersSelectionComponent: PlayerSelectionComponent[]
+    // ): void {
+    //     this.playersSelectionComponent = playersSelectionComponent;
+    // }
 
 }
