39e1bbca5017a2dec1ebc427777f1d3041ccca6f
Jose Daniel <jobustamantea@unal.edu.co>
2026-06-14 18:45:59 -0500

    creación de clases de UI elements

---
+++ game/src/game/UI/Menu/AtomicComponents/BackgrounComponent.ts
@@ -0,0 +1,12 @@
+import UIComponent from "../UIComponent";
+
+export default class BackgroundComponent extends UIComponent {
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, scale: number) {
+        super(scene,x,y,texture,scale);        
+    }
+
+    
+
+
+}
---
+++ game/src/game/UI/Menu/AtomicComponents/TitleComponent.ts
@@ -0,0 +1,10 @@
+import UIComponent from "../UIComponent";
+
+export default class TitleComponent extends UIComponent {
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, scale: number) {
+        super(scene,x,y,texture,scale);        
+    }
+
+    
+}
