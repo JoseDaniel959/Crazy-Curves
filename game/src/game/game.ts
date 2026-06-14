27a9aee090cb0ae9fc82535ecaada8d473c629f5
Jose Daniel <jobustamantea@unal.edu.co>
2026-06-14 18:45:39 -0500

    se crea y se exporta constants

--- game/src/game/game.ts
+++ game/src/game/game.ts
@@ -5,25 +5,36 @@ import Preloader from "./Scenes/Preloader";
 import MainMenu from "./Scenes/MainMenu";
 import MainGame from "./Scenes/MainGame";
 import indexStyles from "../indexStyles.css"
-
 var config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 1000,
-    height: 1000,
+    width: 1500,
+    height: 1200,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     scene: [Boot, Preloader, MainMenu, MainGame],
     physics: {
         default: "arcade",
         arcade: {
-            debug: false
+            debug: true
         }
     },
-    parent: 'phaser-example',
+    parent: 'game',
     dom: {
         createContainer: true,
     },
 };
+
+//css rules of the game
 indexStyles
-let game = new Game(config);
+export const game = new Game(config);
+
+//constants
+export const SCREEN_WIDTH = game.canvas.width;
+export const MID_SCREEN_WIDTH = SCREEN_WIDTH/2;
+
+export const SCREEN_HEIGHT = game.canvas.height;
+
+
+
+
 
 
